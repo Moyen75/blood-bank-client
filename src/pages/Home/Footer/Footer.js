66f2cf649530f7d4121b61970656412290b85638
Moyen75 <moyenislam75@gmail.com>
2022-01-08 16:00:43 +0600

    grid added to footer

--- src/pages/Home/Footer/Footer.js
+++ src/pages/Home/Footer/Footer.js
@@ -1,10 +1,12 @@
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React from 'react';
 
 const Footer = () => {
     return (
         <Box sx={{ paddingX: { xs: '10px', md: '150px' } }}>
-
+               <Grid container spacing={2}>
+                   <Grid item xs={12} md={6}></Grid>
+               </Grid>
         </Box>
     );
 };
